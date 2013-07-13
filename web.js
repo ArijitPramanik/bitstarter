60bfb58ad72865b5a1fd5cbb6840c1d6a9d2a598
Arijit Pramanik <arijit.pramanik@gmail.com>
2013-07-13 13:52:13 +0000

    reading index.html

--- web.js
+++ web.js
@@ -1,9 +1,15 @@
 var express = require('express');
+var fs = require('fs')
+var data_to_write;
+fs.readFile('./index.html', function(err, data){
+	if (err) throw err;
+	data_to_write = data.toString('utf8', 0);
+});
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World 3!');
+  response.send(data_to_write);
 });
 
 var port = process.env.PORT || 5000;
